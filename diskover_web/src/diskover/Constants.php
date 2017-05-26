0e13d14b60b626aeef1a19426562c3fc82f0170e
Chris Park <cpark16@gmail.com>
2017-05-25 19:01:51 -0700

    Create Constants.php

---
+++ diskover_web/src/diskover/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace diskover;
+
+class Constants {
+    const ES_HOST = '192.168.1.72';
+    const ES_INDEX = 'diskover-*';
+    const ES_TYPE = 'file';
+    const ES_USER = 'elastic';
+    const ES_PASS = 'changeme';
+}
