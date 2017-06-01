53f73bdd768d3cb04a1b441540fe92da006caf9e
Chris Park <cpark16@gmail.com>
2017-05-31 18:19:45 -0700

    Update Constants.php

--- diskover_web/src/diskover/Constants.php
+++ diskover_web/src/diskover/Constants.php
@@ -3,10 +3,12 @@
 namespace diskover;
 
 class Constants {
+    // set to your Elasticearch host or ip
     const ES_HOST = 'localhost';
+    // don't change following two lines
     const ES_INDEX = 'diskover-*';
     const ES_TYPE = 'file';
-    // below two lines for X-Pack http-auth
+    // set following two lines if using X-Pack http-auth
     const ES_USER = '';
     const ES_PASS = '';
 }
