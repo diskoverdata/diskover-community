83c758726969d598d8b67ce56afbd9288de70366
Andrey Chausenko <andrey.chausenko@kayosports.com.au>
2019-06-05 16:02:15 +1000

    Added Docker configs

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.7.3-slim-stretch
+
+ENV DISKOVER_WORKDIR /diskover
+ENV DISKOVER_CONFIG ${DISKOVER_WORKDIR}/diskover.cfg
+ENV DISKOVER_ROOTDIR ${DISKOVER_WORKDIR}/rootdir
+ENV DISKOVER_PLUGINDIR ${DISKOVER_WORKDIR}/plugins
+
+WORKDIR ${DISKOVER_WORKDIR}
+
+COPY ./ ${DISKOVER_WORKDIR}/
+
+RUN apt-get update && \
+    apt-get install curlftpfs --yes && \
+    apt-get clean && \
+    mkdir ${DISKOVER_ROOTDIR} && \
+    pip install -r requirements.txt
+
+VOLUME ["${DISKOVER_ROOTDIR}"]
+
+ENTRYPOINT ["/bin/bash", "entrypoint.sh", "--rootdir", "${DISKOVER_ROOTDIR}"]
---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+USE_FTP=
+FTP_USR=anonymous
+FTP_PWD=
+FTP_HOST=127.0.0.1
+FTP_PORT=21
+FTP_PATH=
+DISKOVER_CMD="python diskover.py"
+
+while [ "$1" != "" ]; do
+        case $1 in
+                --ftp-host )            FTP_HOST=$2; shift 2;;
+                --ftp-port )            FTP_PORT=$2; shift 2;;
+                --ftp-path )            FTP_PATH=$2; shift 2;;
+                --ftp-username )        FTP_USR=$2; shift 2;;
+                --ftp-password )        FTP_PWD=$2; shift 2;;
+                --diskover-server )     DISKOVER_CMD="python diskover.py"; shift;;
+                --diskover-worker )     DISKOVER_CMD="python diskover_worker_bot.py"; shift;;
+                --ftp )                 USE_FTP=1; shift;;
+                --)                     shift; break;;
+                * )                     DISKOVER_CMD="$DISKOVER_CMD $1"; shift;;
+        esac 
+done
+
+if [ -z "$USE_FTP" ]; then
+    echo "Will mount ftp://$FTP_HOST:$FTP_PORT/$FTP_PATH on $DISKOVER_ROOTDIR"
+    curlftpfs -r -o custom_list=LIST "ftp://$FTP_USR:$FTP_PWD@$FTP_HOST:$FTP_PORT/$FTP_PATH" "$DISKOVER_ROOTDIR"
+    echo "Will execute: $DISKOVER_CMD"
+    eval "$DISKOVER_CMD"
+    fusermount -u "$DISKOVER_ROOTDIR"
+else
+    eval "$DISKOVER_CMD"
+fi
