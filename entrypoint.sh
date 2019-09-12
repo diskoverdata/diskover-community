d345d63fbbe5b3036ba1ba63d4192afa80bf0f72
Abhijit Kottur <Abhijit.Kottur@foxsports.com.au>
2019-09-12 10:59:40 +1000

    changes for diskover-master. also changes to the entrypoint and configmap

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,23 @@ FTP_HOST=127.0.0.1
 FTP_PORT=21
 FTP_PATH=
 
-if [ "$RUN_MODE" = "WORKER" ]
-then
-    echo "Will START AS SERVER MODE!!!"
-    DISKOVER_CMD='python diskover_worker_bot.py'    
-else if [ "$RUN_MODE" = "TCP_SERVER" ]
-    echo "Will START AS WORKER MODE !!!"
-    DISKOVER_CMD="python diskover.py --listen --debug"
-else     
-fi
+if [ "$RUN_MODE" == "WORKER" ]; then
+    echo "Will run in WORKER mode!!!"
+    DISKOVER_CMD="python diskover_worker_bot.py"
+elif [ "$RUN_MODE" == "SERVER" ]; then
+    echo "Will run in SERVER mode!!!"
+    DISKOVER_CMD="python diskover.py"
 
+    if [ -z "$STANDALONE" ] && [ "$STANDALONE" == "true" ]; then
+        echo "Listening on TCP:9999..."
+        DISKOVER_CMD+=" --listen"
+    elif [ -z "$DEBUG_ENALBED" ] && [ "$DEBUG_ENALBED" == "true" ]; then
+        echo "Debug mode ON"
+        DISKOVER_CMD+=" --debug"
+    fi
+else
+    echo "Unsupported RUN_MODE. Supported values are [WORKER, SERVER] Exiting..."
+fi
 
 echo "Evaluated DISKOVER_CMD=$DISKOVER_CMD"
 
@@ -31,20 +38,7 @@ while [ "$1" != "" ]; do
                 --ftp )                 USE_FTP=1; shift;;
                 --)                     shift; break;;
                 * )                     DISKOVER_CMD="$DISKOVER_CMD $1"; shift;;
-# =======
-#                 --ftp-host )                    FTP_HOST=$2; shift 2;;
-#                 --ftp-port )                    FTP_PORT=$2; shift 2;;
-#                 --ftp-path )                    FTP_PATH=$2; shift 2;;
-#                 --ftp-username )                FTP_USR=$2; shift 2;;
-#                 --ftp-password )                FTP_PWD=$2; shift 2;;
-#                 --diskover-server )             DISKOVER_CMD="python diskover.py"; shift;;
-#                 --diskover-socket-server )      DISKOVER_CMD="python diskover.py --listen --debug"; shift;;
-#                 --diskover-worker )             DISKOVER_CMD="python diskover_worker_bot.py"; shift;;
-#                 --ftp )                         USE_FTP=1; shift;;
-#                 --)                             shift; break;;
-#                 * )                             DISKOVER_CMD="$DISKOVER_CMD $1"; shift;;
-# >>>>>>> CPDBDS-184
-        esac 
+        esac
 done
 
 if ! [ -z "$USE_FTP" ]; then
