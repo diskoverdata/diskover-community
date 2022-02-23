bf763435ce0d579ed07454c849f9b162da02a92e
Chris Park <cpark16@gmail.com>
2022-02-23 00:05:49 -0800

    updated windows-owner plugin

--- diskover/plugins/windows-owner/__init__.py
+++ diskover/plugins/windows-owner/__init__.py
@@ -15,12 +15,13 @@ https://www.diskoverdata.com/solutions/
 
 
 === Plugin Name ===
-diskover Windows owner plugin
+diskover Windows owner/primary group plugin
 
 === Plugin Description ===
 diskover Windows owner plugin - This is an example plugin
-for diskover. It updates owner field meta data of 
-each file or directory to diskover index during indexing.
+for diskover. It updates owner and group fields meta data of 
+each file or directory to diskover index during indexing with
+the Windows owner and primary group info using pywin32.
 
 === Plugin Requirements ===
 pywin32 python module
@@ -36,10 +37,13 @@ all indexing plugins require six functions:
 
 """
 
-version = '0.0.1'
+version = '0.0.2'
 __version__ = version
 
 import win32security
+
+sid_owner_cache = {}
+sid_group_cache = {}
 
 
 def add_mappings(mappings):
@@ -52,8 +56,8 @@ def add_meta(path, osstat):
     """Returns a dict with additional file meta data.
     For any warnings or errors, raise RuntimeWarning or RuntimeError.
     RuntimeWarning and RuntimeError requires two args, error message string and dict or None."""
-    # overwrites owner default index fields with Windows ownership info.
-    return {'owner': get_owner(path)}
+    # overwrites owner and group default index fields with Windows ownership info.
+    return {'owner': get_owner(path), 'group': get_group(path)}
 
 
 def add_tags(metadict):
@@ -89,9 +93,37 @@ def get_owner(filename):
     try:
         sd = win32security.GetFileSecurity(filename, win32security.OWNER_SECURITY_INFORMATION)
         owner_sid = sd.GetSecurityDescriptorOwner()
-        name, domain, type = win32security.LookupAccountSid(None, owner_sid)
+        owner_sid_str = win32security.ConvertSidToStringSid(owner_sid)
+        # check sid cache for owner sid
+        if owner_sid_str in sid_owner_cache:
+            name, domain, type = sid_owner_cache[owner_sid_str]
+        else:
+            # lookup sid and cache values
+            name, domain, type = win32security.LookupAccountSid(None, owner_sid)
+            sid_owner_cache[owner_sid_str] = (name, domain, type)
         owner = domain + '\\' + name
     except Exception as e:
         raise RuntimeWarning('Error getting Windows owner for {0} ({1})'.format(filename, e), None)
     else:
         return owner
+
+
+def get_group(filename):
+    """Like get_owner, but returns primary group."""
+    try:
+        sd = win32security.GetNamedSecurityInfo(filename, win32security.SE_FILE_OBJECT, 
+            win32security.GROUP_SECURITY_INFORMATION)
+        primary_group_sid = sd.GetSecurityDescriptorGroup()
+        group_sid_str = win32security.ConvertSidToStringSid(primary_group_sid)
+        # check sid cache for group sid
+        if group_sid_str in sid_group_cache:
+            name, domain, type = sid_group_cache[group_sid_str]
+        else:
+            # lookup sid and cache values
+            name, domain, type = win32security.LookupAccountSid(None, primary_group_sid)
+            sid_group_cache[group_sid_str] = (name, domain, type)
+        group = domain + '\\' + name
+    except Exception as e:
+        raise RuntimeWarning('Error getting Windows primary group for {0} ({1})'.format(filename, e), None)
+    else:
+        return group
