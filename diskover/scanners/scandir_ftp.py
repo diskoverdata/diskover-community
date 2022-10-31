e8dbdece4e7b689ee8af4f9c3a51415a09f010f4
Sean DiZazzo <sean.dizazzo@gmail.com>
2022-10-31 11:24:51 -0700

    Added copyright info

--- diskover/scanners/scandir_ftp.py
+++ diskover/scanners/scandir_ftp.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+"""
+diskover community edition (ce)
+https://github.com/diskoverdata/diskover-community/
+https://diskoverdata.com
+
+Copyright 2017-2022 Diskover Data, Inc.
+"Community" portion of Diskover made available under the Apache 2.0 License found here:
+https://www.diskoverdata.com/apache-license/
+
+All other content is subject to the Diskover Data, Inc. end user license agreement found at:
+https://www.diskoverdata.com/eula-subscriptions/
+
+Diskover Data products and features for all versions found here:
+https://www.diskoverdata.com/solutions/
+
+"""
 
 import os
 import logging
